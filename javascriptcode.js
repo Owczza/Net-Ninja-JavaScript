5f6f8f85b4ff37db1b14d974f0557d3c5b8a67d5
Owca <37577436+Owczza@users.noreply.github.com>
2018-10-26 22:13:53 +0200

    Else If Statements
    Removed the if/else statements and replaced them with else if statements.

--- javascriptcode.js
+++ javascriptcode.js
@@ -1,14 +1,11 @@
-var youLikeMeat = false;
+var myAge = 29;
 
-if (youLikeMeat) {
-  document.write("You like meat.");
+if (my Age > 30) {
+  document.write("You are over 30.);
+} else if (myAge > 20) {
+  document.write("You are over 20.");
+} else if (myAge > 10 {
+  document.write("You are over 10.);
 } else {
-  document.write("You hate meat.");
-}
-
-var myNum = 8;
-if (myNum == 10){
-  document.write("myNum is equal to 10");
-} else {
-  documenr.write("myNum is not equal to 10.");
+  document.write("You are not over 10.")
 }
