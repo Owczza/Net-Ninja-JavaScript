573b9cd750bf2a7d7dcfc82d8a57c5b9997cc2e0
Owca <37577436+Owczza@users.noreply.github.com>
2018-10-27 01:29:19 +0200

    For loops
    Changed the while loop into a for loop.
    
    Added var links which takes the tag "a" from the html document. The for loop uses the local var i and initiates it at i=1. It will loop as long as i is less than or equal to 6, printing a string and the i that the loop is currently working on. Once the loop breaks, the script prints a string to the document. Since this is a practice for loop and I will most likely be erasing it shortly, I didn't add links to Lessons.html, so it won't run.

--- javascriptcode.js
+++ javascriptcode.js
@@ -1,7 +1,10 @@
-var age = 5
-
-while (age < 10){
-  console.log("Your age is less than 10.");
-  age++;
+for (age = 0; age < 10; age++){
+  console.log("You are under 10.");
 }
 document.write("You are now over 10.");
+
+var links = document.getElementByTagName("a");
+for (i = 1; i <= links.length; i++){
+  console.log("This is the link no." + i)
+}
+document.write("All links now looped.");
