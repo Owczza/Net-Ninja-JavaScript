da5c29570a98f992d0b2b2532f0db1c93cb4af2d
Owca <37577436+Owczza@users.noreply.github.com>
2018-10-26 22:07:19 +0200

    If Else Statements
    Deleted the alert statement and replaced it with two if/else loops.

--- javascriptcode.js
+++ javascriptcode.js
@@ -1,1 +1,14 @@
-alert("hello, World!")
+var youLikeMeat = false;
+
+if (youLikeMeat) {
+  document.write("You like meat.");
+} else {
+  document.write("You hate meat.");
+}
+
+var myNum = 8;
+if (myNum == 10){
+  document.write("myNum is equal to 10");
+} else {
+  documenr.write("myNum is not equal to 10.");
+}
